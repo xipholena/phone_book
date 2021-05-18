262db9c794d708332b76f5c2426a3fbe4f8d6ef0
xipholena <you@example.com>
2021-05-18 09:57:34 +0300

    works with another API, fetch moved to functional expression

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -11,23 +11,22 @@ import {IPerson} from "../components/Book";
 const App/*: FC*/ = () => {
     const [phones, setPhones] = useState([]);
 
-   ;
+   const getPhonesHandler = async () => fetch('https://hn.algolia.com/api/v1/search?query=redux', {
+       headers: {}
+   }) // http://localhost:3000/phones.json  https://hn.algolia.com/api/v1/search?query=redux
+       .then((response) => response.json())
+       .then((data) => setPhones(data.hits))
+       .catch((e) => {
+           console.log(e.toString())
+       })
 
 
     useEffect(() => {
-        fetch('https://hn.algolia.com/api/v1/search?query=redux', {
-            headers: {}
-        }) // http://localhost:3000/phones.json  https://hn.algolia.com/api/v1/search?query=redux
-            .then((response) => response.json())
-            .then((data) => setPhones(data.hits))
-            .catch((e) => {
-                console.log(e.toString())
-            })
+        getPhonesHandler();
     }, [])
 
     useEffect(() => {
         console.log('phones updated, check deps array')
-        //console.log(getPhonesHandler())
     },[phones])
 
 

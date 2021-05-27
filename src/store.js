82580fd104c93bc3249fab9fa1bb835be794d4d8
xipholena <you@example.com>
2021-05-27 10:48:53 +0300

    removed logger from store

--- src/store.js
+++ src/store.js
@@ -1,13 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { logger } from 'redux-logger/src';
 import { rootReducer } from './reducers';
 
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas';
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, sagaMiddleware)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(rootSaga);
 
